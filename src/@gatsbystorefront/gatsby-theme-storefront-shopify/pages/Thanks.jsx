18a705f749692663a42a7d481bdb90abca66e791
Daniela Montanaro <dani.montanaro21@gmail.com>
2020-04-23 14:25:50 -0300

    Trying to create a new custom page

---
+++ src/@gatsbystorefront/gatsby-theme-storefront-shopify/pages/Thanks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@gatsbystorefront/gatsby-theme-storefront-shopify/src/components/Layout';
+
+function Thanks() {
+  return (
+    <Layout>
+      <div>Thanks You!!</div>
+    </Layout>
+  );
+}
+
+export default Thanks;
